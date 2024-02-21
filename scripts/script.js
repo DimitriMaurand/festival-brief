483ab6c7547277dab6c51eddcb4f3c26167ade7c
dimitri <dimitri.mauand.simplon@gmail.com>
2024-02-21 09:04:48 +0100

    départ1

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,9 @@
 
 function suivant() {
     let element = document.getElementById("options");
-    element.style.display = "block";
+    let element2 = document.getElementById("reservation");
+    element.style.display = "flex";
+    element2.style.display = "none";
     element.style.transition = "opacity 2s ease";
 
 }
@@ -13,7 +15,34 @@ bouton.addEventListener("click", suivant);
 
 function suivant2() {
     let element = document.getElementById("coordonnees");
-    element.style.display = "block";
+    let element2 = document.getElementById("options");
+    element.style.display = "flex";
+    element2.style.display = "none";
 }
 let bouton2 = document.querySelector(".bouton2");
 bouton2.addEventListener("click", suivant2);
+
+//apparition en cochant
+
+// Récupère la case à cocher
+const checkbox = document.getElementById('tarifReduit');
+const checkboxEnfantsOui = document.getElementById('enfantsOui');
+// Récupère la div
+const divTarifReduit = document.querySelector('.tarifReduit');
+const sectionEnfant = document.querySelector('.enfant');
+
+checkbox.addEventListener('change', () => {
+    if (checkbox.checked) {
+        divTarifReduit.style.display = 'block';
+    } else {
+        divTarifReduit.style.display = 'none';
+    }
+});
+
+checkboxEnfantsOui.addEventListener('change', () => {
+    if (checkboxEnfantsOui.checked) {
+        sectionEnfant.style.display = 'block';
+    } else {
+        sectionEnfant.style.display = 'none';
+    }
+});
