6d96a4f5e576e513450bedfef68ac1a7acb79cde
dimitri <dimitri.mauand.simplon@gmail.com>
2024-02-28 17:05:54 +0100

    mercredi

--- confirmation.php
+++ confirmation.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
-require 'php/src/classes/Client.php';
-require 'php/src/classes/Database.php';
-require 'php/src/traitement.php';
+require_once 'php/src/classes/Client.php';
+require_once 'php/src/classes/Database.php';
+require_once 'php/src/traitement.php';
 // if (isset($_SESSION['connecté']) && !empty($_SESSION['user'])) {
 //     // abort
 //     header('location:tableau-de-bord.php');
@@ -30,12 +30,12 @@ if (isset($_GET['erreur'])) {
     <link rel="stylesheet" href="./styles/style.css">
 </head>
 <form action="src/authentication.php" method="post" onsubmit=" return ValidationConnexion()">
-    <h1>Connexion</h1>
+    <h1>confirmation</h1>
     <div>
         <?php
         $DB = new Database();
         $utilisateurs = $DB->getAllClients();
-        include 'includes/section-utilisateurs.php';
+        include 'include/section-utilisateurs.php';
         ?>
     </div>
 
@@ -45,17 +45,7 @@ if (isset($_GET['erreur'])) {
             Votre inscription est validée !
         </div>
     <?php } ?>
-    <label for="mail">Mail :</label>
-    <input type="email" name="mail" id="mail" required>
-    <label for="password">Mot de passe :</label>
-    <input type="password" name="password" id="password" required>
-    <div id="message"></div>
-    <?php if ($echec) { ?>
-        <div class="message echec">
-            Mot de passe ou email invalide.
-        </div>
-    <?php } ?>
-    <input type="submit" value="Se connecter">
+
 </form>
 <script src="./scripts/script.js"></script>
 
